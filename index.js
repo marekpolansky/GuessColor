93534a9c075223fdbe7edb1204a86528231b79e0
Marek Polasnky <marek.polansky@student.tuke.sk>
2023-01-07 17:46:38 +0100

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let rgbRef = document.querySelector('.rgb');
+let btn1Ref = document.getElementById('btn1');
 let [rgbnum1,rgbnum2,rgbnum3] = [0,0,0];
 rgbnum1 = Math.floor(Math.random() * 150) + 1;
 rgbnum2 = Math.floor(Math.random() * 150) + 1;
 rgbnum3 = Math.floor(Math.random() * 150) + 1;
-document.getElementById('.btn1').bgcolor= "#000000";
+document.getElementById('btn1').style.backgroundColor= '#000000';
 rgbRef.innerText = 'RGB(' + rgbnum1 + ',' + rgbnum2 + ',' + rgbnum3 + ')';
