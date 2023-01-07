835a16435de148db30e773e696c7afae82b3c91f
Marek Polasnky <marek.polansky@student.tuke.sk>
2023-01-07 17:52:43 +0100

    second commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let [rgbnum1,rgbnum2,rgbnum3] = [0,0,0];
 rgbnum1 = Math.floor(Math.random() * 150) + 1;
 rgbnum2 = Math.floor(Math.random() * 150) + 1;
 rgbnum3 = Math.floor(Math.random() * 150) + 1;
-document.getElementById('btn1').style.backgroundColor= '#000000';
+document.getElementById('btn1').style.backgroundColor= "#"+(rgbnum1).toString(16)+(rgbnum2).toString(16)+(rgbnum3).toString(16);
 rgbRef.innerText = 'RGB(' + rgbnum1 + ',' + rgbnum2 + ',' + rgbnum3 + ')';
