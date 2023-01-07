432f586e750424c3ea87fa61f4e08d61ce696032
Peter Skok - VSC <ptskokmine@gmail.com>
2023-01-07 19:59:40 +0100

    debugged js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 let rgbRef = document.querySelector('.rgb');
 let btn1Ref = document.getElementById('btn1');
 let [rgbnum1,rgbnum2,rgbnum3] = [0,0,0];
-let num;
-rgbnum1 = Math.floor(Math.random() * 155) + 1;
-rgbnum2 = Math.floor(Math.random() * 155) + 1;
-rgbnum3 = Math.floor(Math.random() * 155) + 1;
-num = Math.floor(Math.random() * 3) + 1;
+rgbnum1 = Math.floor(Math.random() * 256);
+rgbnum2 = Math.floor(Math.random() * 256);
+rgbnum3 = Math.floor(Math.random() * 256);
+let num = Math.floor(Math.random() * 3) + 1;
 if (num == 1){
-    document.getElementById('btn1').style.backgroundColor= "#"+(rgbnum1).toString(16)+(rgbnum2).toString(16)+(rgbnum3).toString(16);
-    document.getElementById('btn2').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
-    document.getElementById('btn3').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
+    document.getElementById('btn1').style.backgroundColor= "rgb(" + rgbnum1 + ", " + rgbnum2 + ", " + rgbnum3 + ")";
+    document.getElementById('btn2').style.backgroundColor= getRandomRgb()
+    document.getElementById('btn3').style.backgroundColor= getRandomRgb()
 }
 else if (num == 2){
-    document.getElementById('btn2').style.backgroundColor= "#"+(rgbnum1).toString(16)+(rgbnum2).toString(16)+(rgbnum3).toString(16);
-    document.getElementById('btn1').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
-    document.getElementById('btn3').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
+    document.getElementById('btn2').style.backgroundColor= "rgb(" + rgbnum1 + ", " + rgbnum2 + ", " + rgbnum3 + ")";
+    document.getElementById('btn1').style.backgroundColor= getRandomRgb()
+    document.getElementById('btn3').style.backgroundColor= getRandomRgb()
 }
 else if (num == 3){
-    document.getElementById('btn3').style.backgroundColor= "#"+(rgbnum1).toString(16)+(rgbnum2).toString(16)+(rgbnum3).toString(16);
-    document.getElementById('btn1').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
-    document.getElementById('btn2').style.backgroundColor= "#"+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16)+(Math.floor(Math.random() * 155) + 1).toString(16);
+    console.log(document.getElementById('btn3').style.backgroundColor)
+    document.getElementById('btn3').style.backgroundColor= "rgb(" + rgbnum1 + ", " + rgbnum2 + ", " + rgbnum3 + ")";
+    document.getElementById('btn1').style.backgroundColor= getRandomRgb()
+    document.getElementById('btn2').style.backgroundColor= getRandomRgb()
+}
+
+function getRandomRgb(){
+    return "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")"; 
 }
 
 rgbRef.innerText = 'RGB(' + rgbnum1 + ',' + rgbnum2 + ',' + rgbnum3 + ')';
